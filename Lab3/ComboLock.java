ad80ebb191c8ddc62a296d4cbca9098f96464812
Rylan Yancey <rylany37@gmail.com>
2022-02-02 10:47:10 -0500

    Update ComboLock.java

--- Lab3/ComboLock.java
+++ Lab3/ComboLock.java
@@ -8,7 +8,10 @@ public class ComboLock
    private int currentNumber = 0; //current value lock dial is set to
    //more variables here ....
 
+   // Stores secrets in an array so we can iterate them
    int[] secrets = {0, 0, 0};
+
+   // tells us how far along we are
    int secrets_unl = 0; 
 
    /**
@@ -26,6 +29,7 @@ public class ComboLock
    // (the 45 gets clamped to 39 because it was > 39).
    public ComboLock(int secret1, int secret2, int secret3)
    {
+      // Initialize and Clamp secret numbers. 
       secrets[0] = clamp(39, 0, secret1);
       secrets[1] = clamp(39, 0, secret2);
       secrets[2] = clamp(39, 0, secret3);
@@ -36,11 +40,8 @@ public class ComboLock
    //********* COMPLETE THIS METHOD
    public void reset()
    {
+      // Resets the currentNumber and Secrets unl so we can try again. 
       currentNumber = 0;
-
-      secrets[0] = 0;
-      secrets[1] = 0;
-      secrets[2] = 0;
       secrets_unl = 0;
    }
 
@@ -54,11 +55,14 @@ public class ComboLock
    //number we started on
    public void turnRight(int ticks)
    {
+      // Iterates down the number of ticks
       currentNumber -= ticks;
 
+      // Correction for if you go under zero, wraps around
       if (currentNumber < 0) 
          currentNumber = currentNumber + 40;
 
+      // Checks for if you found one of the secrets. 
       if (currentNumber == secrets[secrets_unl]) 
          secrets_unl++;
    }
@@ -73,10 +77,14 @@ public class ComboLock
    //number we started on
    public void turnLeft(int ticks)
    {
+      // Increase the ticks
 	   currentNumber += ticks;
+
+      // Correction for if you go over, wraps around
       if (currentNumber > 39) 
          currentNumber = currentNumber - 40;
 
+      // Checks if you've found the secret. 
       if (currentNumber == secrets[secrets_unl])
          secrets_unl++;
    }
@@ -88,6 +96,7 @@ public class ComboLock
    //**** COMPLETE THIS METHOD
    public boolean open()
    {
+      // Checks if you've found all the secrets. 
       if (secrets_unl == 3) return true;
       else return false;
    }
@@ -99,6 +108,13 @@ public class ComboLock
 	   return currentNumber;
    }
 
+   /**
+    * <p> Clamps the value to the specified range. 
+    * @param max => the maximum value 
+    * @param min => the minimum value
+    * @param num => the value in question
+    * @return
+    */
    public int clamp(int max, int min, int num) {
 
       if (num > max) return max;
