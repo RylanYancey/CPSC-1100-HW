d7c2e2e37a2ad30e46bdeea2e1f2b34b20334f4d
Rylan Yancey <rylany37@gmail.com>
2022-02-15 15:04:36 -0500

    Create Circuit.java

---
+++ Lab4/Circuit.java
@@ -0,0 +1,8 @@
+
+/*
+    Master class that makes a promise that everything that inherits it will have the GetResistance() method. 
+*/
+
+public class Circuit {
+    public float GetResistance() { return 0.0f; };
+}
