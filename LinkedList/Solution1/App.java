b28eae8afcab5489b3953837c860fcdfc660ec80
Rylan Yancey <rylany37@gmail.com>
2022-03-01 15:14:38 -0500

    Create App.java

---
+++ LinkedList/Solution1/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedList link = new LinkedList();
+        link.Insert(5);
+        link.Insert(10);
+        link.Insert(3);
+        link.Insert(2);
+        link.Display();
+    }
+}
