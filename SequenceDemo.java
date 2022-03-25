e4993ab17896a9d7bfacf509d5adc4b144b97717
Rylan Yancey <rylany37@gmail.com>
2022-03-25 10:40:31 -0500

    Added all files.

---
+++ SequenceDemo.java
@@ -0,0 +1,20 @@
+public class SequenceDemo
+{
+   public static void main(String[] args)
+   {
+      LastDigitDistribution dist1 = new LastDigitDistribution();
+      dist1.process(new SquareSequence(), 1000);
+      dist1.display();
+      System.out.println();
+
+      LastDigitDistribution dist2 = new LastDigitDistribution();
+      dist2.process(new RandomSequence(), 1000);
+      dist2.display();
+      System.out.println();
+
+      LastDigitDistribution dist3 = new LastDigitDistribution();
+      dist3.process(new PrimeSequence(), 1000);
+      dist3.display();  
+      System.out.println();
+   }
+}
